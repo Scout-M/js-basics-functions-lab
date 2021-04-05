96f5e5717608e1fd2b81213471712f5d21867014
Amy Mangor <aemangor@gmail.com>
2021-04-05 19:19:33 -0400

    commit tree fdcd4330aefa229a5355e076cc5ba973439490c5

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickUp) {
+  const headquarters = 42;
+  if (pickUp < headquarters) {
+    return headquarters - pickUp;
+  }
+  return pickUp - headquarters;
+}
+
+function distanceFromHqInFeet(pickUp) {
+  const blocks = distanceFromHqInBlocks(pickUp);
+  return blocks * 264;
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+  if (startingBlock < endingBlock) {
+    return (endingBlock - startingBlock) * 264;
+  }
+  return (startingBlock - endingBlock) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const feet = distanceTravelledInFeet(start, destination);
+  if (feet <= 400) {
+    return 0;
+  } else if (feet <= 2000) {
+    return (feet - 400) * 0.02;
+  } else if (feet > 2000 && feet < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
